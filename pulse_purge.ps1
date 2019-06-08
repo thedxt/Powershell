aec5a9e64e06d8077b1980ec81f90dd868fda239
Daniel Keer <4249262+thedxt@users.noreply.github.com>
2019-06-08 09:01:25 -0600

    Update pulse_purge.ps1

--- pulse_purge.ps1
+++ pulse_purge.ps1
@@ -2,4 +2,4 @@ Set-Service "PC Monitor" -StartupType Disabled
 Stop-Service "PC Monitor"
 sc.exe delete "PC Monitor"
 sc.exe delete "PulsewayDeploymentService"
-Remove-Item –path "C:\Program Files\Pulseway\" –recurse -force
+Remove-Item -path "C:\Program Files\Pulseway\" -recurse -force
