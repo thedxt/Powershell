238b709e5f6290f71ca4186bdb161af5676f072b
thedxt <4249262+thedxt@users.noreply.github.com>
2019-06-29 14:02:50 -0600

    Create FilePerms.ps1
    First Version

---
+++ Single Task/FilePerms.ps1
@@ -0,0 +1,30 @@
+﻿#set folder permissions
+#Folder can be either UNC or mounted drive
+#enter the groups as DOMAIN\GROUP NAME
+#author theDXT
+#last update 29 June 2019
+
+#defining factors
+$folder = "E:\Shares\"
+$FCGroup = "DOAMIN\Something - FC"
+$ROGroup = "DOAMIN\Something - RO"
+$RWGroup = "DOAMIN\Something - RW"
+
+#scripy mcscript stuff
+$systemgroup = "SYSTEM"
+$permissions = Get-Acl -path $folder
+$permissions.SetAccessRuleProtection($true,$false)
+$InheritanceFlag = @([System.Security.AccessControl.InheritanceFlags]::ContainerInherit,[System.Security.AccessControl.InheritanceFlags]::ObjectInherit) 
+$PropagationFlag = [System.Security.AccessControl.PropagationFlags]::None
+$SystemRule = New-Object System.Security.AccessControl.FileSystemAccessRule($systemgroup,'FullControl',$InheritanceFlag,$PropagationFlag,"Allow")
+$FCRule = New-Object System.Security.AccessControl.FileSystemAccessRule($FCGroup,'FullControl',$InheritanceFlag,$PropagationFlag,"Allow")
+$RORule = New-Object System.Security.AccessControl.FileSystemAccessRule($ROGroup,'ReadAndExecute',$InheritanceFlag,$PropagationFlag,"Allow")
+$RWRule = New-Object System.Security.AccessControl.FileSystemAccessRule($RWGroup,'Modify',$InheritanceFlag,$PropagationFlag,"Allow")
+$permissions.AddAccessRule($SystemRule)
+$permissions.AddAccessRule($FCRule)
+$permissions.AddAccessRule($RORule)
+$permissions.AddAccessRule($RWRule)
+Write-Host "setting permissions on $folder please wait if you have lots of files this will take a bit"
+$permissions | set-acl
+Write-Host "The setting of the permissions has completed for $folder"
+(get-acl -Path $folder).Access.IdentityReference
