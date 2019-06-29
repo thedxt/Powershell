e777802f65ad9028a04611dca7af11f4ac83aea0
thedxt <4249262+thedxt@users.noreply.github.com>
2019-06-29 12:15:18 -0600

    Create DKDefaults.ps1
    Turns on Remote Dekstop, File and Printer sharing

---
+++ Single Task/DKDefaults.ps1
@@ -0,0 +1,6 @@
+﻿#DK Defaults
+#Turns on Remote Dekstop, File and Printer sharing
+Set-NetFirewallRule -DisplayGroup "File and Printer Sharing*" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Remote Desktop*" -Enabled True
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" –Value 0
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name "UserAuthentication" –Value 1
