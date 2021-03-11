adcdf283635a83e8cd33b0457432e0e55229048e
Daniel Keer <4249262+thedxt@users.noreply.github.com>
2021-03-11 15:48:04 -0700

    Update pulse_purge.ps1

--- Single Task/pulse_purge.ps1
+++ Single Task/pulse_purge.ps1
@@ -1,5 +1,26 @@
+#kill service
 Set-Service "PC Monitor" -StartupType Disabled
 Stop-Service "PC Monitor"
+
+#kill processes
+Get-Process | Where {$_.ProcessName -Like "PCMonitorSrv"} | Stop-Process -force
+Get-Process | Where {$_.ProcessName -Like "pcmontask"} | Stop-Process -force
+
+
+#kill regs
+Remove-Item -Path 'HKLM:\SOFTWARE\MMSOFT Design' -Recurse
+
+#find version
+$VersionFinder = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -match 'Pulseway'}
+msiexec.exe /x $VersionFinder.PSChildName /qn
+
+#kill folders
+Remove-Item "C:\Program Files\Pulseway" -Recurse -Force -ErrorAction silentlycontinue
+
+# kill shortcuts
+Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Pulseway Manager.lnk"
+Remove-Item "C:\Users\Public\Desktop\Pulseway Manager.lnk"
+
+#delete service
 sc.exe delete "PC Monitor"
 sc.exe delete "PulsewayDeploymentService"
-Remove-Item -path "C:\Program Files\Pulseway\" -recurse -force
