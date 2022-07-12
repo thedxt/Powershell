4c8a9c90c7dacf984c384f348326daf38eb67a0a
Daniel Keer <4249262+thedxt@users.noreply.github.com>
2022-07-12 08:51:37 -0600

    Update SMB_Open_Finder.ps1
    added line to close open files. commented out by default

--- Single Task/SMB_Open_Finder.ps1
+++ Single Task/SMB_Open_Finder.ps1
@@ -5,5 +5,9 @@
 # change this to define what open file or folder you are looking for
 $what = "pdf"
 
-# searches list the open files
+# searches and lists the open files
 Get-SmbOpenFile | Where-Object -Property Path -like *$what* | select clientusername, path | FT
+
+# close the open files
+# Uncomment the line below if you want it to close the open files
+#Get-SmbOpenFile | Where-Object -Property Path -like *$what* | Close-SmbOpenFile -Force
