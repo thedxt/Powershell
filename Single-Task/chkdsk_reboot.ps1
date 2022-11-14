393e877201125534be9df42d0cc83e7c99a39f37
Daniel Keer <4249262+thedxt@users.noreply.github.com>
2022-11-14 12:08:17 -0700

    Update chkdsk_reboot.ps1
    added in sleep to slow it down to allow the job to finish

--- Single-Task/chkdsk_reboot.ps1
+++ Single-Task/chkdsk_reboot.ps1
@@ -1,6 +1,7 @@
-﻿# tell the system to run chkdsk on C and says yes to running it on next reboot
+# tells the system to run chkdsk on C and says yes to running it on next reboot
 $check_C = Start-Job -Name chkdsk_c -ScriptBlock {echo 'y'|chkdsk.exe c: /r}
 $check_C
-
+# added sleep to let it finish scheduling the job before rebooting
+sleep -Seconds 60
 # reboot
 shutdown -r -f -t 0
