6949b17a8a0b68675c6e726b510d20a239006708
Daniel Keer <4249262+thedxt@users.noreply.github.com>
2022-11-14 13:20:22 -0700

    Add files via upload
    sfc scannow

---
+++ Single-Task/sfc_scannow_reboot.ps1
@@ -0,0 +1,11 @@
+﻿write-host "Starting SFC scan"
+# run sfc scan now and reboot
+$sfc = Start-Job -Name sfc -ScriptBlock {sfc /scannow}
+$sfc
+#wait for the job to be done
+wait-job -Name sfc
+write-host "SFC scan done"
+#add a sleep just to be safe
+sleep -Seconds 60
+# reboot
+shutdown -r -f -t 0
