925dc1cd2cc509495ebd84a35967e1086a4aa618
Daniel Keer <4249262+thedxt@users.noreply.github.com>
2022-11-14 11:57:05 -0700

    Add files via upload

---
+++ Single-Task/chkdsk_reboot.ps1
@@ -0,0 +1,6 @@
+﻿# tell the system to run chkdsk on C and says yes to running it on next reboot
+$check_C = Start-Job -Name chkdsk_c -ScriptBlock {echo 'y'|chkdsk.exe c: /r}
+$check_C
+
+# reboot
+shutdown -r -f -t 0
