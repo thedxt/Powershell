c75668cc357335f18c77e94d5c7e2ded738f5f9c
Daniel Keer <4249262+thedxt@users.noreply.github.com>
2024-10-08 22:02:24 -0600

    adding gpcache nuke
    added nuking for gpcache reg key

--- Single-Task/Nuke_WSUS.ps1
+++ Single-Task/Nuke_WSUS.ps1
@@ -1,3 +1,4 @@
 Stop-Service -Name wuauserv
 Remove-Item HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate -Recurse
+Remove-Item HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UpdatePolicy\GPCache -Recurse
 Start-Service -name wuauserv
